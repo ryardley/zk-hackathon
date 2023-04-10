342bde320d913ee341ca2555ce7d93dfbfde1221
ouromoros <ouromoros@gmail.com>
2023-04-10 21:21:20 +0800

    PadV 17393 constraints

--- keccak256-circom/test/keccak-intermediate.js
+++ keccak256-circom/test/keccak-intermediate.js
@@ -40,6 +40,8 @@ describe("Keccak-PadV test", function () {
 
     it ("PadV (testvector generated from go)", async () => {
 	const cir = await wasm_tester(path.join(__dirname, "circuits", "padv_test.circom"));
+	await cir.loadConstraints();
+	console.log("constraints:", cir.constraints.length);
 
 	const input =
 	    [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
