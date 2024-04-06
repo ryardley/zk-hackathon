6493387e9c063aecb01761a098ca61e30d121930
ryardley <contact@rudiyardley.com>
2024-04-06 14:54:25 +1100

    Add nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+
+{
+  description = "Development environment with package list";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let 
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in 
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          # package list
+          buildInputs = [ circom ];
+        };
+      }    
+    );
+}
