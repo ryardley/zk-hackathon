0eb25bda0e5bef4233dc29d19b49a18406357100
ryardley <contact@rudiyardley.com>
2024-04-06 16:52:51 +1100

    Add circom-lsp

--- flake.nix
+++ flake.nix
@@ -1,25 +1,40 @@
-
 {
-  description = "Development environment with package list";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }: 
     flake-utils.lib.eachDefaultSystem (system:
       let 
+        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
-          inherit system;
+          inherit system overlays;
         };
-      in 
+        circom-lsp = pkgs.rustPlatform.buildRustPackage {
+          name = "circom-lsp";
+          version = "0.1.3";
+          src = pkgs.fetchFromGitHub {
+            owner = "rubydusa";
+            repo = "circom-lsp";
+            rev = "v0.1.3";
+            sha256 = "";
+          };
+          cargoSha256 = "";
+        };
+      in
       with pkgs;
       {
         devShells.default = mkShell {
-          # package list
-          buildInputs = [ circom ];
+          buildInputs = [
+            rust-bin.beta.latest.default
+            rust-analyzer
+            circom-lsp
+            circom
+          ];
         };
-      }    
+        
+      }
     );
 }
